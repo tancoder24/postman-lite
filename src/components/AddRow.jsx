9726bab2b288d127bd7cdfafb019e118f8f04469
tancoder24 <tanmaygupta2408@gmail.com>
2022-06-05 20:53:06 +0530

    table body working

--- src/components/AddRow.jsx
+++ src/components/AddRow.jsx
@@ -1,15 +1,23 @@
 import { TableRow, TableCell } from "@mui/material";
-import { makeStyles } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
-
+const useStyles = makeStyles({
+  tableCell: {
+    border: "1px solid rgba(224, 224, 224, 1)",
+    borderCollapse: "collapse",
+    padding: ["7px 5px", "!important"]
+}
+});
 
 const AddRow = () => {
     
+    const classes = useStyles();
+
     return (
       <TableRow>
-        <TableCell >1</TableCell>
-        <TableCell >2</TableCell>
-        <TableCell >3</TableCell>
+        <TableCell className={classes.tableCell} >1</TableCell>
+        <TableCell className={classes.tableCell} >2</TableCell>
+        <TableCell className={classes.tableCell} >3</TableCell>
       </TableRow>
     )
 };
